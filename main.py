0e3cfd52533bcf07b7aab5eea266a8cdcf1fe95c
Manoj Gugulothu <gugulothumanoj58@gmail.com>
2025-04-14 23:56:03 +0530

    Updated requirements.txt and added new features

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ def play_game():
     if "secret_number" not in session:
         session["secret_number"] = random.randint(1, 20)
         session["guesses"] = []
-      
+        session["score"] = 20
       
 
     message = ""
     guesses = session.get("guesses", [])
+    score = session.get("score", 20)
+
 
     if request.method == "POST":
         if request.form.get("action") == "reset":
             session["secret_number"] = random.randint(1, 20)
             session["guesses"] = []
+            session["score"] = 20
             return redirect(url_for("play_game"))
 
         try:
@@ -30,19 +33,22 @@ def play_game():
 
             if guess < session["secret_number"]:
                 message = "Too low! 🔽 Try a bit higher."
+                session["score"] -= 1
             elif guess > session["secret_number"]:
                 message = "Too high! 🔼 Maybe go lower."
+                session["score"] -= 1
             else:
                 message = f"Congratulations🎉 You guessed it! The number was {session['secret_number']} in {len(guesses)} guesses. 🔥"
                 session["play_sound"] = True
                 session["secret_number"] = random.randint(1, 20)
                 session["guesses"] = []
+                session["score"] = 20
 
         except ValueError:
             message = "Invalid input. Please enter a number."
 
     play_sound = session.pop("play_sound", False)
-    return render_template("index.html", message=message, guesses=guesses, play_sound=play_sound)
+    return render_template("index.html", message=message, guesses=guesses, play_sound=play_sound, score=session["score"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
